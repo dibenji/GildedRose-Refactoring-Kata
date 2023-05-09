e8755133882c1aa83b4007f14697f1e7ed3591ac
Josip Di Benedetto <josip.dibenedetto@ost.ch>
2023-05-09 13:49:22 +0200

    changed quality variable of item-class to quality_value

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -8,40 +8,40 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
+                if item.quality_value > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                        item.quality_value = item.quality_value - 1
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
+                if item.quality_value < 50:
+                    item.quality_value = item.quality_value + 1
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
                         if item.sell_within_days < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                            if item.quality_value < 50:
+                                item.quality_value = item.quality_value + 1
                         if item.sell_within_days < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                            if item.quality_value < 50:
+                                item.quality_value = item.quality_value + 1
 
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_within_days = item.sell_within_days - 1
             if item.sell_within_days < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
+                        if item.quality_value > 0:
                             if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                                item.quality_value = item.quality_value - 1
                     else:
-                        item.quality = item.quality - item.quality
+                        item.quality_value = item.quality_value - item.quality_value
                 else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    if item.quality_value < 50:
+                        item.quality_value = item.quality_value + 1
 
 
 class Item:
-    def __init__(self, name, sell_in, quality):
+    def __init__(self, name, sell_within_days, quality_value):
         self.name = name
-        self.sell_within_days = sell_in
-        self.quality = quality
+        self.sell_within_days = sell_within_days
+        self.quality_value = quality_value
 
     def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_within_days, self.quality)
+        return "%s, %s, %s" % (self.name, self.sell_within_days, self.quality_value)
