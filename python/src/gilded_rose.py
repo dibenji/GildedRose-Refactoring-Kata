a30d20735d0c26ae389684c0e70f9a4a15ed1477
Josip Di Benedetto <josip.dibenedetto@ost.ch>
2023-05-09 21:23:45 +0200

    changed gilded_rose.py to use increase_quality() and decrease_quality() instead of direct access to quality variable

--- python/src/gilded_rose.py
+++ python/src/gilded_rose.py
@@ -21,17 +21,17 @@ class GildedRose(object):
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                        GildedRose.decrease_quality(item)
             else:
                 if item.quality < 50:
-                    item.quality = item.quality + 1
+                    GildedRose.increase_quality(item)
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
                         if item.sell_in < 11:
                             if item.quality < 50:
-                                item.quality = item.quality + 1
+                                GildedRose.increase_quality(item)
                         if item.sell_in < 6:
                             if item.quality < 50:
-                                item.quality = item.quality + 1
+                                GildedRose.increase_quality(item)
 
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
@@ -40,9 +40,9 @@ class GildedRose(object):
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
                         if item.quality > 0:
                             if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                                GildedRose.decrease_quality(item)
                     else:
                         item.quality = item.quality - item.quality
                 else:
                     if item.quality < 50:
-                        item.quality = item.quality + 1
+                        GildedRose.increase_quality(item)
